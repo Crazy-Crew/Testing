794f8063e467db1544705c4035443784cd5e3f40
Ryder Belserion <contact@ryderbelserion.com>
2025-01-06 22:35:16 -0500

    Update App.java
    Signed-off-by: Ryder Belserion <contact@ryderbelserion.com>

--- src/main/java/com/ryderbelserion/App.java
+++ src/main/java/com/ryderbelserion/App.java
@@ -2,6 +2,6 @@ package com.ryderbelserion;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! This is a test!");
+        System.out.println("Hello World! This is another test!");
     }
 }
