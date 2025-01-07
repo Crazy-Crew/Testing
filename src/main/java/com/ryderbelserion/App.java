ae9cd9192308117595000f89e3155ffbdabdcab6
Ryder Belserion <contact@ryderbelserion.com>
2025-01-06 21:02:12 -0500

    Add another println
    Signed-off-by: Ryder Belserion <contact@ryderbelserion.com>

--- src/main/java/com/ryderbelserion/App.java
+++ src/main/java/com/ryderbelserion/App.java
@@ -3,5 +3,7 @@ package com.ryderbelserion;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! This is a test!");
+
+        System.out.println("My name is beanie the rabbit")
     }
 }
