ba55d39f577867265445d861372b34580de6b3f6
Ryder Belserion <contact@ryderbelserion.com>
2025-01-06 21:06:10 -0500

    Add missing semicolon
    Signed-off-by: Ryder Belserion <contact@ryderbelserion.com>

--- src/main/java/com/ryderbelserion/App.java
+++ src/main/java/com/ryderbelserion/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! This is a test!");
 
-        System.out.println("My name is beanie the rabbit")
+        System.out.println("My name is beanie the rabbit");
     }
 }
